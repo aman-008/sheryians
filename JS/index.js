55f9aca8160a58361f8f14cd420dc0f4567041eb
aman-008 <amanp9568@gmail.com>
2026-05-04 18:03:53 +0530

    html and css

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+let prompt = require('prompt-sync')();
+
+unit = 101;
+res = unit * 6;
+console.log(res.toFixed(1));
---
+++ JS/math-function.js
@@ -0,0 +1,36 @@
+// conver into positive
+console.log(Math.abs(-10));
+
+// convert into upper number
+console.log(Math.ceil(10.2));
+// convert into lower number
+console.log(Math.floor(10.8));
+// convert upper if greater than or equal to .5 otherwise lower
+console.log(Math.round(13.5));
+
+
+// remove the decimal part
+console.log(Math.trunc(10.3432424));
+
+
+// return power
+console.log(Math.pow(2,5));
+
+
+// return max value
+console.log(Math.max(12,434,657,8684,534));
+// return min value
+console.log(Math.min(12,434,-657,8684,534));
+
+// return random value bw 0 to 1
+console.log(Math.random());
+
+// return fixed decimal number
+let a = 34343.121343;
+console.log(a.toFixed(2))
+
+// return square root
+console.log(Math.sqrt(16));
+// return cube root
+console.log(Math.cbrt(27));
+
